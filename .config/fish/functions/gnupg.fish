70b395fd752bc6c3c0624cc5f7cc4d4076e035ff
Clayton Burlison <git@clburlison.com>
2018-03-13 09:46:03 -0500

    fix: Update fish gpg commands for v2.1

--- .config/fish/functions/gnupg.fish
+++ .config/fish/functions/gnupg.fish
@@ -1,30 +1,8 @@
 # Start or re-use a gpg-agent.
 #
-# Assumes gpg-agent writes its environment info in ~/.gpg-agent-info.
+# Requires gpgtools v2.1+
 #
-# Sets the gpg-agent environment stuff as universal variables, so it
-# takes effect across all shells.
-#
-# Taken from https://github.com/kvs/dotfiles/blob/master/fish/gnupg.fish
+# Taken from http://deferred.io/2017/08/03/yubikey4-gpg-ssh-u2f.html
 
-function __refresh_gpg_agent_info -d "Re-load ~/.gpg-agent-info into environment"
-	cat ~/.gpg-agent-info | sed 's/=/ /' | while read key value
-		set -e $key
-		set -U -x $key "$value"
-	end
-end
-
-if not set -q -x GPG_AGENT_INFO
-	gpg-agent --daemon >/dev/null 2>&1
-end
-
-if test -f ~/.gpg-agent-info
-	__refresh_gpg_agent_info
-
-	gpg-connect-agent /bye ^/dev/null 2>&1
-	if test $status -eq 1
-		pkill -U $USER gpg-agent 2>&1
-		gpg-agent --daemon >/dev/null 2>&1
-		__refresh_gpg_agent_info
-	end
-end
+gpg-connect-agent /bye
+export SSH_AUTH_SOCK=(gpgconf --list-dirs agent-ssh-socket)
